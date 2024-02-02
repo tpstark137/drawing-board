cb6f1f67bad7abe88580772eb89b627659328035
tpstark137 <tpaul.be21@gmail.com>
2024-02-02 16:35:05 +0530

    Toolbox UI

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,8 +1,10 @@
 import Menu from "@/components/Menu";
+import Toolbox from "@/components/Toolbox";
 export default function Home() {
   return (
     <div>
       <Menu/>
+      <Toolbox/>
     </div>
   );
 }
