57070f5df0f40ec4e418bf158356d3bebcc2fb82
tpstark137 <tpaul.be21@gmail.com>
2024-02-02 15:58:42 +0530

    Menu UI

---
+++ src/components/Menu/index.js
@@ -0,0 +1,27 @@
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faPencil , faEraser , faRotateLeft , faRotateRight , faFileArrowDown } from '@fortawesome/free-solid-svg-icons'
+
+import styles from './index.module.css'
+const Menu = () =>{
+    return(
+        <div className={styles.menuContainer}>
+            <div className={styles.wrapper}>
+            <FontAwesomeIcon icon={faPencil} className={styles.icon}/>
+            </div>
+            <div className={styles.wrapper}>
+            <FontAwesomeIcon icon={faEraser} className={styles.icon}/>
+            </div>
+            <div className={styles.wrapper}>
+            <FontAwesomeIcon icon={faRotateLeft} className={styles.icon}/>
+            </div>
+            <div className={styles.wrapper}>
+            <FontAwesomeIcon icon={faRotateRight} className={styles.icon}/>
+            </div>
+            <div className={styles.wrapper}>
+            <FontAwesomeIcon icon={faFileArrowDown} className={styles.icon}/>
+            </div>
+        </div>
+    )
+}
+
+export default Menu;
